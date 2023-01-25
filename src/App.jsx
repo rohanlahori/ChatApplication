ad1aa4c33b96fdb22cbd0ecff95c93a9c14381b9
Rohan Lahori <rohanlahori99@gmail.com>
2023-01-25 17:12:42 +0530

    frontend working

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
 import Cookies from 'universal-cookie'
-import {ChannelContainer,ChannelListContainer} from './components';
+import {ChannelContainer,ChannelListContainer,Auth} from './components';
 import './App.css'
 const apiKey='8xusjvvcgup6'
 const client=StreamChat.getInstance(apiKey)
-const authToken=false;
 
+const authToken=false;
 
 const App = () => {
 
-  // if(!authToken)
-  // {
-  //   return <Auth/>
-  // }
+  if(!authToken)
+  {
+    return <Auth/>
+  }
   return (
     <div className='app__wrapper'>
         <Chat client={client} theme='team light'>
